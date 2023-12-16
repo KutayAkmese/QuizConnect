23d8545757df87eb0072629bb03f56d75aad8d92
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2023-12-16 20:22:00 +0300

    Add models into admin panel

--- quizconnect/app/admin.py
+++ quizconnect/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Lesson, Question, Answer, TimeLineItem
+
+admin.site.register(User)
+admin.site.register(Lesson)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(TimeLineItem)
+
