13a238e0f728a46efd14ac4c3bbff4ab7cf566a8
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2023-12-16 20:23:23 +0300

    Update database settings

--- quizconnect/quizconnect/settings.py
+++ quizconnect/quizconnect/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = "quizconnect.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'QuizConnect', 
-        #'USER': 'postgres',
-        #'PASSWORD': 'Berkan188138',
-        #'HOST': 'localhost', 
-        #'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'QuizConnect',
+        'USER': 'postgres',
+        'PASSWORD': 'Berkan188138',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
