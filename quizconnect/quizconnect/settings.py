f7e87dfba299e3affbf6bfe6eef0eff28ef0c848
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2024-01-03 22:53:07 +0300

    Change db settings by local berkan's db

--- quizconnect/quizconnect/settings.py
+++ quizconnect/quizconnect/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = "quizconnect.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
+        'NAME': 'QuizConnect',
         'USER': 'postgres',
-        'PASSWORD': '-Afb2cGDaGfEDga1FGA31CdB6cBge*g-',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '51949'
+        'PASSWORD': 'Berkan188138',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
